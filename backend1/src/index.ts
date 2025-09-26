5c59cc50a40cad1bf709cefea95772f1b25b9eee
Samarkun <samarkun4@gmail.com>
2025-09-26 23:07:18 +0530

    starter code

---
+++ backend1/src/index.ts
@@ -0,0 +1,33 @@
+import { WebSocketServer } from 'ws'
+import { GameManager } from './GameManager.js';
+
+const wss = new WebSocketServer({ port: 3000 });
+
+const gameManager = new GameManager();
+
+wss.on('connection', function connection(ws) {
+    gameManager.addUser(ws)
+    ws.on("disconnect", () => gameManager.removeUser(ws))
+})
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
