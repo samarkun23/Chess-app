b25119d8385e309feb6fec215a4d8e22448df1a8
samarkun <samarkun4@gmail.com>
2026-03-18 23:47:41 +0530

    making the connection on the backend

--- apps/frontend/src/hook/useSocket.ts
+++ apps/frontend/src/hook/useSocket.ts
@@ -3,26 +3,22 @@ import { useEffect, useState } from "react"
 
 const WS_URL = "http://localhost:3000"
 export const useSocket = () => {
-    const [socket , setSocket] = useState<WebSocket | null>();
-    
+    const [socket, setSocket] = useState<WebSocket | null>();
+
     useEffect(() => {
 
         const ws = new WebSocket(WS_URL);
 
         ws.onopen = () => {
-            setSocket(ws) 
+            console.log("Connected");
+            setSocket(ws)
         }
-
-        ws.onclose = () => {
-            setSocket(null)
-        }
-
         return () => {
             ws.close();
         }
 
-    },[])
+    }, [])
 
-
+    console.log("SOCKET STATE", socket)
     return socket;
 }
