4ee5da0ba7332cd54204827c604359fe26508b0f
samarkun <samarkun4@gmail.com>
2026-03-18 23:44:46 +0530

    add the validate point , fix the signIN point fix the signUp point

--- apps/backend/src/auth/index.ts
+++ apps/backend/src/auth/index.ts
@@ -1,5 +1,5 @@
-import {prismaClient} from '@repo/db/client'
-import {  Router } from 'express'
+import { prismaClient } from '@repo/db/client'
+import { Router } from 'express'
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import dotenv from 'dotenv'
@@ -9,15 +9,15 @@ dotenv.config();
 
 const router = Router()
 
-router.post("/signup", async(req,res) => {
+router.post("/signup", async (req, res) => {
     const body = req.body;
     console.log(JWT_SECRET)
 
     const inUserExists = await prismaClient.user.findFirst({
         where: {
-            OR:[
-                {email: body.email},
-                {username: body.username}
+            OR: [
+                { email: body.email },
+                { username: body.username }
             ]
         }
     })
@@ -25,26 +25,26 @@ router.post("/signup", async(req,res) => {
     if (inUserExists) {
         return res.status(409).json({
             message: "This user allready exists"
-        }) 
+        })
     }
 
     try {
         const hashedPassword = await bcrypt.hash(body.password, 10);
         const createUser = await prismaClient.user.create({
-            data:{
+            data: {
                 email: body.email,
-                username : body.username,
+                username: body.username,
                 password: hashedPassword
             }
         })
         console.log("User is created")
-        
+
         return res.status(201).json({
             id: createUser.id,
             email: createUser.email,
             username: createUser.username
         });
-        
+
 
     } catch (error) {
         console.error(error);
@@ -55,57 +55,79 @@ router.post("/signup", async(req,res) => {
 
 })
 
-router.post("/signin", async(req,res) => {
+router.post("/signin", async (req, res) => {
     const body = req.body;
 
-    let user ;
-    if(body.email){
+    let user;
+    if (body.email) {
         user = await prismaClient.user.findUnique({
-            where: {email: body.email}
+            where: { email: body.email }
         })
-    }else if(body.username){
+    } else if (body.username) {
         user = await prismaClient.user.findUnique({
-            where: {username : body.username}
+            where: { username: body.username }
         })
     }
 
     if (!user) {
         return res.status(400).json({
             message: "User not exits pz first signup"
-        }) 
+        })
     }
 
     try {
         const isPasswordValid = await bcrypt.compare(
             body.password,
-            user.password 
-        ) 
+            user.password
+        )
 
         if (!isPasswordValid) {
             return res.status(403).json({
                 message: "user did not exits"
-            }) 
+            })
         }
 
-        const token = jwt.sign({ userId: user.id}, JWT_SECRET!);
+        const token = jwt.sign({ userId: user.id }, JWT_SECRET!);
 
         res.cookie('token', token, {
             httpOnly: true,
-            secure: true,
-            sameSite: 'lax'
+            secure: process.env.NODE_ENV === "production",
+            sameSite: 'strict'
         });
 
         res.json({
             message: "LOGIN SUCCESSFULLY"
         })
     } catch (error) {
-        res.status(500).json({message: "Error while singIn"}) 
+        res.status(500).json({ message: "Error while singIn" })
     }
 })
 
-router.post("/logout", (req,res) => {
+router.get("/validate", async (req, res) => {
+    const token = req.cookies.token;
+
+    if (!token) {
+        return res.json(401).json({ message: "No token provided" })
+    }
+
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET) as { userId: any };
+        const user = await prismaClient.user.findFirst({
+            where: { id: decoded.userId },
+            select: { id: true, username: true, email: true }
+        })
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+        return res.json({ user, token });
+    } catch (error) {
+        res.status(401).json({ message: "Invalid token" })
+    }
+})
+
+router.post("/logout", (req, res) => {
     res.clearCookie("token");
-    res.json({message: "Logged out"});
+    res.json({ message: "Logged out" });
 })
 
 export const authRouter = router
