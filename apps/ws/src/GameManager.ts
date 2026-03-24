c99cd4a0dd6e112cd13d79d0b435a3be3b4939c1
samarkun <samarkun4@gmail.com>
2026-03-24 23:11:12 +0530

    add the auth

--- apps/ws/src/GameManager.ts
+++ apps/ws/src/GameManager.ts
@@ -1,11 +1,11 @@
 import {WebSocket} from "ws";
 import { INIT_GAME, MOVE } from "./messages.js";
 import { Game } from "./Game.js";
-
+import { AuthWebSocket } from "./middleware/authMiddleware.js";
 
 export class GameManager{
     private games: Game[];
-    private pendingUser: WebSocket | null; // a user who is currently waiting to be connected 
+    private pendingUser: {id: number, socket: WebSocket} | null; // a user who is currently waiting to be connected 
     private users: WebSocket[]; // list of active user on server
 
     constructor() {
@@ -14,9 +14,13 @@ export class GameManager{
         this.users = [];
     }
 
-    addUser(socket: WebSocket) {
+    addUser(socket: AuthWebSocket) {
+        if(!socket.userId){
+            socket.close()
+            return;
+        }
         this.users.push(socket)
-        this.addHandler(socket)
+        this.addHandler(socket, Number(socket.userId))
     }
 
     removeUser(socket: WebSocket){
@@ -25,7 +29,10 @@ export class GameManager{
 
     }
     
-    private addHandler(socket: WebSocket) {
+    private addHandler(socket: WebSocket,userId: number) {
+
+        const user = {id:userId ,socket};
+
         socket.on("message", (data) => {
 
             const message = JSON.parse(data.toString());
@@ -33,17 +40,18 @@ export class GameManager{
             if(message.type === INIT_GAME){
                 if(this.pendingUser){
                     // start game 
-                    const game = new Game(this.pendingUser, socket);
+                    const game = new Game(this.pendingUser, user);
                     this.games.push(game);
                     this.pendingUser = null;
+                    game.initGame();
                 }else{
-                    this.pendingUser = socket;
+                    this.pendingUser = user;
                 }
             }
 
             if(message.type === MOVE){
                 console.log(message.type , "This is the move")
-                const game = this.games.find(game => game.player1 === socket || game.player2 === socket);
+                const game = this.games.find(game => game.player1.socket === socket || game.player2.socket === socket);
                 if(game){
                     console.log("inside makemove");
                     game.makeMove(socket, message.payload.move);
